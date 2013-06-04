1e7f8833d1117d60d6195e839dbe3dfeb8570db0
philmaweb <philmaweb@gmail.com>
2013-06-04 23:53:05 +0200

    Commit Nr n...

---
+++ T-Zell-Reaktivity/src/Main.java
@@ -0,0 +1,13 @@
+import org.shogun.*;
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.loadLibrary("modshogun");
+		modshogun.init_shogun_with_defaults();
+	}
+
+}
